e6ca975f6f3a5f901be04ef95a872a484ed5a028
Aleksandr <chykh@mail.ru>
2023-03-20 21:43:09 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1  [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,27 +1,19 @@
 package ru.job4j.grabber;
-
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
-
 import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class HabrCareerParse {
 
     private static final String SOURCE_LINK = "https://career.habr.com";
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
         for (int i = 1; i <= 5; i++) {
-            Connection connection = Jsoup.connect(PAGE_LINK + "?page=" + i);
-            Document document = connection.get();
-            Elements rows = document.select(".vacancy-card__inner");
             System.out.println("\nPAGE " + i);
-            rows.forEach(row -> {
+            Jsoup.connect(PAGE_LINK + i).get().select(".vacancy-card__inner").forEach(row -> {
                 Element titleElement = row.select(".vacancy-card__title").first();
                 Element linkElement = titleElement.child(0);
                 String vacancyName = titleElement.text();
