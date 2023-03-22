1d0ca6ce49c6e0234e4d9e97077d1e9ff6425ac2
Aleksandr <chykh@mail.ru>
2023-03-23 01:32:58 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -10,6 +10,16 @@ public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
+    public static String retrieveDescription(String link) {
+        Element descriptionElement;
+        try {
+            descriptionElement = Jsoup.connect(link).get().select(".style-ugc").first();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return descriptionElement != null ? descriptionElement.text() : null;
+    }
+
     public static void main(String[] args) throws IOException {
         for (int i = 1; i <= 5; i++) {
             System.out.println("\nPAGE " + i);
@@ -18,11 +28,13 @@ public class HabrCareerParse {
                 Element linkElement = titleElement.child(0);
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+
                 Element dateElement = row.select(".vacancy-card__date").first();
                 String date = dateElement.child(0).attr("datetime");
                 HabrCareerDateTimeParser hcdtp = new HabrCareerDateTimeParser();
                 LocalDateTime lcd = hcdtp.parseDate(date);
                 System.out.printf("%s %s %s%n", vacancyName, link, lcd);
+                System.out.println(HabrCareerParse.retrieveDescription(link) + "\n");
             });
         }
     }
