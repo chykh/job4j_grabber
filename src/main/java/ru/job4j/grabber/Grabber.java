bfcdf4a033629a8740a69cd86379100422ae646f
Aleksandr <chykh@mail.ru>
2023-04-25 06:31:30 +0300

    Вспомогательный

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -4,6 +4,122 @@ import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+    private Parse parse;
+    private Store store;
+    private Scheduler scheduler;
+    private int time;
+    Properties cfg = new Properties();
+
+    public Grabber() {
+    }
+
+    public Store cfg() throws IOException {
+        try (InputStream in = Grabber.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            cfg.load(in);
+        }
+        time = Integer.parseInt(cfg.getProperty("time"));
+        return new PsqlStore(cfg);
+    }
+
+   @Override
+    public void start() throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(time)
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public void start(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(time)
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+    }
+
+    public static class GrabJob implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            try {
+                var list = parse.list("https://career.habr.com/vacancies/java_developer?page=");
+                for (Post post : list) {
+                    store.save(post);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public static void main(String[] args) throws Exception {
+         Grabber grab = new Grabber();
+         Store store = grab.cfg();
+         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+         scheduler.start();
+         grab.start(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+         grab.web(store);
+    }
+}
+
+/*
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
+import java.io.*;
 import java.util.Properties;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
@@ -71,3 +187,4 @@ public class Grabber implements Grab {
         new Grabber(parse, store, scheduler, time).start();
     }
 }
+ */
