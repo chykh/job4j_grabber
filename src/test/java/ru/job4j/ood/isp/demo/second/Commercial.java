ca58a4c99c9b7333371267617e684f56f643c143
Aleksandr <chykh@mail.ru>
2023-10-29 05:35:20 +0300

    0. Принцип разделения интерфейсов [#4916] - 2

---
+++ src/test/java/ru/job4j/ood/isp/demo/second/Commercial.java
@@ -0,0 +1,12 @@
+package ru.job4j.ood.isp.demo.second;
+
+public interface Commercial extends LeaseAgreement2 {
+
+    public boolean setDate();
+
+    public boolean setOGRN();
+
+    public boolean setFounder();
+
+    public boolean setFirmName();
+}
---
+++ src/test/java/ru/job4j/ood/isp/demo/second/SelfEmployed.java
@@ -0,0 +1,24 @@
+package ru.job4j.ood.isp.demo.second;
+
+public class SelfEmployed implements Private {
+
+    @Override
+    public boolean setDate() {
+        return false;
+    }
+
+    @Override
+    public boolean setName() {
+        return false;
+    }
+
+    @Override
+    public boolean setBirthDay() {
+        return false;
+    }
+
+    public boolean setSelfEmployedCert() {
+        return false;
+    }
+
+}
